ec1d825bcd26e06c19b8c2900a13f87aee17108e
lucas <moraislcs05@gmail.com>
2025-03-06 21:37:34 -0300

    aula 05 - funcoes e objetos

---
+++ projetos/gerenciamentoReservas/pseudocodigo.md
@@ -0,0 +1,17 @@
+INICIAR lista de reservas com array vazio
+
+FUNÇÃO adicionarReserva(nome, data)
+    Criar objeto "reserva" com nome e data
+    Adicionar objeto ao array de reservas
+    Atualizar exibicao na tela
+
+FUNÇÃO removerReserva(index)
+    Remover reserva no índice informado
+    Atualizar exibicao na tela
+
+FUNÇÃO atualizarListaReservas()
+    Limpar a exibição na tela
+    Para cada reserva na lista:
+        Criar um item visual para exibição
+        Criar botão de remoção para cada reserva
+        Exibir na tela
---
+++ projetos/gerenciamentoReservas/script.js
@@ -0,0 +1,37 @@
+let reservas = [];
+
+function addReserva() {
+
+    let nome = document.getElementById("name").value;
+    let data = document.getElementById("date").value;
+    console.log(`Nova reserva adicionada para ${nome} no dia ${data}`);
+    if (nome === "" || data === "") {
+        alert("Preencha todos os campos");
+        return;
+    }
+
+    let novaReserva = { nome, data };
+    reservas.push(novaReserva);
+
+    document.getElementById("name").value = "";
+    document.getElementById("date").value = "";
+
+    attListaReserva();
+    
+}
+
+function removerReserva(index) {
+    reservas.splice(index, 1);
+    attListaReserva();
+}
+
+function attListaReserva() {
+    let lista = document.getElementById("listaReservas");
+    lista.innerHTML = "";
+
+    reservas.forEach((reserva, index) => {
+        let li = document.createElement("li");
+        li.innerHTML = `${reserva.nome}, ${reserva.data} <button onclick="removerReserva(${index})">X</button>`;
+        lista.appendChild(li);
+    });
+}
